e558e3e6ff433994d702a2438bbc2135efecc554
deoresulakshana <deoresulakshana@gmail.com>
2023-11-14 11:10:54 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <label runat="server">Enter First Number</label>
+            <asp:TextBox runat="server"  ID="txtfno" />
+            <br />
+
+            <label runat="server">Enter Second Number</label>
+            <asp:TextBox runat="server"  ID="txtsno" />
+            <br />
+            
+            <br />
+            <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+            <br />
+            <asp:TextBox ID="txtans" runat="server"></asp:TextBox>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)//not going to the server for the next time/second time/ ie I am visiting first time
+            {
+                Response.Write("Loading complete...... Welcome .... U have come here first time.....");
+            }
+            else
+            {
+                Response.Write("Thanks for revisiting.....");
+            }
+            
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            int ans = Convert.ToInt32(txtfno.Text) + Convert.ToInt32(txtsno.Text);
+            txtans.Text = ans.ToString();
+
+        //    txtans.Text = (Convert.ToInt32(txtfno.Text) + Convert.ToInt32(txtsno.Text)).ToString();
+             
+
+        }
+    }
+}
